5acf2e1f8a52d9658eb9e2350f5d11822952e014
Aditya Uday Ubale <51261116+aditya25022001@users.noreply.github.com>
2021-03-23 10:10:28 +0530

    ''

--- Python4Sem.py
+++ Python4Sem.py
@@ -1,28 +1,32 @@
-import math
+# import math
 
-# Question 1 input name and print with hello
-name = input("What is your name ? ")
-age = int(input("What is your age ? "))
-print("Hello " + name + " your age after five years : " + 5+age)
+# # Question 1 input name and print with hello
+# name = input("What is your name ? ")
+# age = int(input("What is your age ? "))
+# print("Hello " + name + " your age after five years : " + 5+age)
 
-# Area of circle
-diameter = float(input("diameter of circle : "))
-area = math.pi*(diameter**2)/4
-print(area)
+# # Area of circle
+# diameter = float(input("diameter of circle : "))
+# area = math.pi*(diameter**2)/4
+# print(area)
 
-# Print numbers from 0 to 10
-for x in range(11):
-    print(x)
+# # Print numbers from 0 to 10
+# for x in range(11):
+#     print(x)
 
-# ask name from user
-name = input("Enter name : ")
-if(name):
-    print("Welcome "+name)
-else:
-    print("Welcome unknown")
+# # ask name from user
+# name = input("Enter name : ")
+# if(name):
+#     print("Welcome "+name)
+# else:
+#     print("Welcome unknown")
 
-# Print all perfect squares in range of 1 - 1000
-a = 1
-while(a**2 < 1000):
-    print(a**2)
-    a += 1
+# # Print all perfect squares in range of 1 - 1000
+# a = 1
+# while(a**2 < 1000):
+#     print(a**2)
+#     a += 1
+
+# 1-1000
+for i in range(1, 1001, 2):
+    print(i)
