944a8f3377c91f13dc5c78dcdebf56470a6bf1a6
Aditya Uday Ubale <51261116+aditya25022001@users.noreply.github.com>
2021-03-31 16:24:14 +0530

    done

--- Python4Sem.py
+++ Python4Sem.py
@@ -1,51 +1,57 @@
-import math
+# import math
 
-# Question 1 input name and print with hello
-name = input("What is your name ? ")
-age = int(input("What is your age ? "))
-age+=5
-print("Hello " + name + " your age after five years : " + age)
+# # Question 1 input name and print with hello
+# name = input("What is your name ? ")
+# age = int(input("What is your age ? "))
+# age+=5
+# print("Hello " + name + " your age after five years : " + age)
 
-# Area of circle
-diameter = float(input("diameter of circle : "))
-area = math.pi*(diameter**2)/4
-print(area)
+# # Area of circle
+# diameter = float(input("diameter of circle : "))
+# area = math.pi*(diameter**2)/4
+# print(area)
 
-# Print numbers from 0 to 10
-for x in range(11):
-    print(x)
+# # Print numbers from 0 to 10
+# for x in range(11):
+#     print(x)
 
-# ask name from user
-name = input("Enter name : ")
-if(name):
-    print("Welcome "+name)
-else:
-    print("Welcome unknown")
+# # ask name from user
+# name = input("Enter name : ")
+# if(name):
+#     print("Welcome "+name)
+# else:
+#     print("Welcome unknown")
 
-# Print all perfect squares in range of 1 - 1000
-a = 1
-while(a**2 < 1000):
-    print(a**2)
-    a += 1
+# # Print all perfect squares in range of 1 - 1000
+# a = 1
+# while(a**2 < 1000):
+#     print(a**2)
+#     a += 1
 
-# 1-1000
-for i in range(1, 1001, 2):
-    print(i)
+# # 1-1000
+# for i in range(1, 1001, 2):
+#     print(i)
 
-# Print type of each value in list
-list = [1, 2, 'geeks', 4, 6, 'geeks']
-for each in list:
-    print(type(each))
+# # Print type of each value in list
+# list = [1, 2, 'geeks', 4, 6, 'geeks']
+# for each in list:
+#     print(type(each))
 
-# create list of table 10
-list = [10*x for x in range(1, 13)]
-print(list)
-list2 = ['hello', 'geeks']
-list.append(list2)
-print(list)
-print(list[-1])
-print(list[-1][-1])
-print(len(list))
-print(list[len(list)-1])
-print(list[len(list)-1][len(list2)-1])
-# #
+# # create list of table 10
+# list = [10*x for x in range(1, 13)]
+# print(list)
+# list2 = ['hello', 'geeks']
+# list.append(list2)
+# print(list)
+# print(list[-1])
+# print(list[-1][-1])
+# print(len(list))
+# print(list[len(list)-1])
+# print(list[len(list)-1][len(list2)-1])
+
+# # Python extend method
+list1 = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+list2 = []
+for each in list1:
+    list2.extend(each)
+print(list2)
